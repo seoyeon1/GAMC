884945166bffb33cd18d4e3d22dab8e44ef9bd6c
seoyeon1 <okyeon1@naver.com>
2022-10-13 23:57:24 +0900

    [복습] 1주차 정리

---
+++ week1/review/r1.js
@@ -0,0 +1,25 @@
+// 1주차 1번 다시풀기 (JS)
+
+// Run by Node.js
+const readline = require('readline');
+let data = [];
+(async () => {
+	let rl = readline.createInterface({ input: process.stdin });
+	
+	for await (const line of rl) {
+		data.push(line);
+		// console.log(data);
+		// rl.close();
+	}
+	console.log(solution(data));
+	process.exit();
+})();
+
+function solution(arr){
+	// let n = data[0];
+	let bridges = data[1].split(' ');
+	// console.log(bridges);
+	
+	let res = bridges.reduce((a,b)=> a*(+b), 1);
+	return res;
+}
